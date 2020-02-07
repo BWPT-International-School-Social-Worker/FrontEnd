92f29fd4f1422e5985d2c9ae163319344bdf8cbd
Anna Morris <annapatton444@gmail.com>
2020-02-06 20:29:07 -0600

    Revert "Michael luck"

--- school-social-worker/src/components/StudentInfo.jsx
+++ school-social-worker/src/components/StudentInfo.jsx
@@ -1,25 +1,20 @@
-import React, { useContext } from "react";
-import { studentContext } from "../contexts/studentContext";
+import React,{useContext} from 'react'
+import {studentContext} from "../contexts/studentContext"
 
-function StudentInfo(props) {
-  const { students } = useContext(studentContext);
-  console.log(students);
-  const student =
-    students &&
-    students.find(kid => {
-      return `${kid.id}` === props.match.params.id;
-    });
+ function StudentInfo(props) {
+    const {students}=useContext(studentContext);
+    console.log(students);
+const student = students && students.find(kid =>{
+   return `${kid.id}` === props.match.params.id
+});
 
-  console.log(student);
+console.log(student);
 
-  return (
-    <div className="student-info-container">
-      <img src="https://iconsetc.com/icons-watermarks/flat-circle-white-on-yellow/bfa/bfa_user/bfa_user_flat-circle-white-on-yellow_512x512.png" />
-      <h1>
-        {student && student.first_name} {student && student.last_name}
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{student && student.id}</h1>
+        </div>
+    )
 }
 
 export default StudentInfo;
