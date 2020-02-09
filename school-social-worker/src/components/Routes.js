ee1486554308596c4d20a73a16069702e21b8659
Anna Morris <annapatton444@gmail.com>
2020-02-08 19:44:33 -0600

    routes

---
+++ school-social-worker/src/components/Routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Landing from "./Landing";
+import Navigation from "./Navigation";
+import PrivateRoute from "./PrivetRoute";
+
+function Routes() {
+  return (
+    <div>
+      <Router>
+        routes
+        <Route exact path="/">
+          <Landing></Landing>
+        </Route>
+        <PrivateRoute exact path="/home">
+          <Navigation></Navigation>
+        </PrivateRoute>
+      </Router>
+    </div>
+  );
+}
+
+export default Routes;
