461ecf319aeb33e01251a3171fa9fa5c7dd6f18b
Anna Morris <annapatton444@gmail.com>
2020-02-08 19:46:23 -0600

    student card

--- school-social-worker/src/components/StudentCard.js
+++ school-social-worker/src/components/StudentCard.js
@@ -1,28 +1,22 @@
-import React, { useEffect, useState } from "react";
 import "./StudentCard.scss";
-import { Link } from "react-router-dom";
-import EditStudentForm from "./EditStudentForm";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
 export default function StudentCard(props) {
-  const history = useHistory();
-  const routeToPage = () => {
-    history.push(`/student-info/${props.kid.id}`);
-  };
-  console.log(props);
+  const student = props.student;
+  console.log(student);
   return (
-    <ul className="student-card" key={props.kid.id}>
-      <img src="https://iconsetc.com/icons-watermarks/flat-circle-white-on-yellow/bfa/bfa_user/bfa_user_flat-circle-white-on-yellow_512x512.png" />
+    <div>
+      <ul className="student-card" key={props.student.id}>
+        {/* <img src="https://iconsetc.com/icons-watermarks/flat-circle-white-on-yellow/bfa/bfa_user/bfa_user_flat-circle-white-on-yellow_512x512.png" /> */}
 
-      <li>
-        {props.kid.first_name} {props.kid.last_name}
-      </li>
-      <span>
-        <li>Grade {props.kid.grade_id}</li>
-      </span>
-      <li>Status: {props.kid.status}</li>
-      <button onClick={() => routeToPage()}>More Info</button>
-      <EditStudentForm id={props.kid.id} student={props.kid} />
-    </ul>
+        <li>
+          {props.student.first_name} {props.student.last_name}
+        </li>
+        <span>
+          <li>Grade {props.student.grade_id}</li>
+        </span>
+        <li>Status: {props.student.status}</li>
+      </ul>
+    </div>
   );
 }
